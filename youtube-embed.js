0f46357ecc3bd4a9c5007c4f6d5a5733d3fdaaa5
pkpaperinik <pkpaperinik@gmail.com>
2025-12-25 19:13:22 +0100

    Update youtube-embed.js

--- youtube-embed.js
+++ youtube-embed.js
@@ -26,7 +26,7 @@ class YoutubeEmbed extends HTMLElement {
 <iframe
   width="${this.width}"
   height="${this.height}"
-  src="https://www.youtube-nocookie.com/embed/${this.videoid}"
+  src="https://www.youtube-nocookie.com/embed/${this.videoid}&autoplay=1"
   srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${this.videoid}?autoplay=1></a>"
   frameborder="0"
   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
